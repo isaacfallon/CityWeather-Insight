b263c315856f165fcc6d1a870ac670648a59c198
ZACGIL <zac.gilfoyle@gmail.com>
2024-04-18 18:15:40 +1000

    implemented better search results by providing a description array in JS, and also added classes to HTML to display them

--- assets/wikipediaAPI.js
+++ assets/wikipediaAPI.js
@@ -1,54 +1,64 @@
 // Wikipedia API logic here
 
 // ?Get HTML DOM references
-const form = document.querySelector('.search-box');
-const input = document.querySelector('#search-term');
 const resultsContainer = document.querySelector('.results');
-const resultsCounter = document.querySelector('#counter');
+const remainderContainer = document.querySelector('.remainder')
+// const resultsCounter = document.querySelector('.counter');
 
 // ?Variables
-const userSearch = input.value;
+const exampleOpenWeatherDesc = "thunderstorm";
+const weatherDesc = JSON.parse(localStorage.getItem("weatherDesc")) || exampleOpenWeatherDesc;
+const descriptionsArr = ["sky", "clouds", "rain", "thunderstorm", "snow", "mist"];
+const remainderArr = [];
 
-const exampleOpenWeatherDesc = "shower rain";
-
-const descriptionsArray = ["sky", "clouds", "rain", "thunderstorm", "snow", "mist"];
-
-// ?'Submit' listener for text box - executes search function
-form.addEventListener('submit', function (event){
+// ? Load this content on page load 
+document.addEventListener("DOMContentLoaded", function (event) {
     event.preventDefault();
-    searchWiki(findSearchTerm(exampleOpenWeatherDesc));
-    // ?Check if input is empty and then execute function
-    // if(userSearch){
-    //     searchWiki(findSearchTerm(exampleOpenWeatherDesc));
-    // }
-});
+
+    awaitResults();
+})
+
+async function awaitResults() {
+    let result = await searchWiki(findSearchTerm(weatherDesc));
+    displayResults(result, resultsContainer);
+
+    let arr = [];
+    for await (const term of remainderArr) {
+        let remainder = await searchWiki(term);
+        arr.push(remainder[0]);
+    }    
+    displayResults(arr, remainderContainer);
+}
 
 function findSearchTerm(searchTerm) {
 
-    descriptionsArray.forEach(desc =>{
-        if(searchTerm.includes(desc)){
+    descriptionsArr.forEach(desc => {
+        if (searchTerm.includes(desc)) {
             searchTerm = desc;
+        } else {
+            remainderArr.push(desc);
         }
     });
 
+    console.log(remainderArr);
     console.log(searchTerm);
     return searchTerm;
 }
 
 // ?Function for search wiki that is asynchronous, meaning it will execute code whilst also waiting for an operation to complete
-async function searchWiki(searchTerm){
+async function searchWiki(searchTerm) {
     // ?Get Wiki API and set query parameters
     //const urlPrevious = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${encodeURIComponent(searchTerm)}`;
     const endpoint = "https://en.wikipedia.org/w/api.php";
     const parameters = "?action=query"
-    + "&list=search" // which list to get, in this case a full text search 
-    + "&prop=info" // which properties to get from queried pages, in this case, basic page information
-    + "&inprop=url" // which additional properties to get - meta info, in this case also the URL
-    + "&utf8=" 
-    + "&format=json" 
-    + "&origin=*" // omitting this causes a CQRS error
-    + "&srlimit=1" // search limit on list equal to a number
-    + "&srsearch="; // 
+        + "&list=search" // which list to get, in this case a full text search 
+        + "&prop=info" // which properties to get from queried pages, in this case, basic page information
+        + "&inprop=url" // which additional properties to get - meta info, in this case also the URL
+        + "&utf8="
+        + "&format=json"
+        + "&origin=*" // omitting this causes a CQRS error
+        + "&srlimit=1" // search limit on list equal to a number
+        + "&srsearch="; // 
     const search = `${encodeURIComponent(searchTerm)}`;
 
     const wikiURL = endpoint + parameters + search;
@@ -63,31 +73,30 @@ async function searchWiki(searchTerm){
     let response = await fetch(wikiURL).catch(error => alert('Error : ' + error));;
     // ?Store data as JSON when response is received
     let data = await response.json();
-    // ?Asynchronous function that waits till data is received, takes it in as a parameter and then executes a function that displays it
-    let result = async(d) => {displayResults(d.query.search)};
-    // ?Call function and feed in data as the parameter
-    result(data);
+    // ?Get search array fron data
+    let result = data.query.search;
+    // ?Return the result
+    return result;
 }
 
-// ?Function that displays the results of the wiki search, the parameter is an array
-function displayResults(results){
+// ?Function that displays the results of the wiki search, the parameter is an array and a container to display it in
+async function displayResults(results, container) {
     // ?Clear results container
-    resultsContainer.innerHTML = '';
+    console.log(results);
+    container.innerHTML = '';
     // ?Set the text of the results count
-    resultsCounter.textContent = `Results Count : ${results.length}`;
+    // resultsCounter.textContent = `Results Count : ${results.length}`;
     // ?Create an element for each result and append them to the container
     results.forEach(result => {
         const resultElement = document.createElement('div');
-        resultElement.className = 'result';
+        resultElement.className = 'result box has-text-left';
         resultElement.innerHTML = `
         
-        <h3>${result.title}</h3>
-        <p class="text-left">${result.snippet}</p>
-        <a href="https://en.wikipedia.org/?curid=${result.pageid}" target="_blank">Read More</a>
+        <h4 class="title is-4">${result.title}</h4>
+        <p class="content">${result.snippet}</p>
+        <button class="button is-primary" onclick="window.open('https://en.wikipedia.org/?curid=${result.pageid}','_blank')">Read More</button>
         `;
-        resultsContainer.appendChild(resultElement);    
+        container.appendChild(resultElement);
     });
 }
-
-
 
